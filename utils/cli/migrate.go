048555a2fe64aa521b2b18f78cd3152cd811885d
weebagency <cedricm@weebagency.com>
2017-08-26 19:46:46 -0400

    add examples

--- utils/cli/migrate.go
+++ utils/cli/migrate.go
@@ -2,4 +2,15 @@ package cli
 
 func Seed() {
 	// @TODO - implement
+	/*
+			tx := db.MustBegin()
+		    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "Jason", "Moiron", "jmoiron@jmoiron.net")
+		    tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", "John", "Doe", "johndoeDNE@gmail.net")
+		    tx.MustExec("INSERT INTO place (country, city, telcode) VALUES ($1, $2, $3)", "United States", "New York", "1")
+		    tx.MustExec("INSERT INTO place (country, telcode) VALUES ($1, $2)", "Hong Kong", "852")
+		    tx.MustExec("INSERT INTO place (country, telcode) VALUES ($1, $2)", "Singapore", "65")
+		    // Named queries can use structs, so if you have an existing struct (i.e. person := &Person{}) that you have populated, you can pass it in as &person
+		    tx.NamedExec("INSERT INTO person (first_name, last_name, email) VALUES (:first_name, :last_name, :email)", &Person{"Jane", "Citizen", "jane.citzen@example.com"})
+			tx.Commit()
+	*/
 }
